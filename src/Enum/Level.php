646550262e62d10e06ad559080152e1fa7eb3452
Michal Molenda <contact@codeapps.io>
2022-05-06 12:52:59 +0200

    All levels in enum class

--- src/Enum/Level.php
+++ src/Enum/Level.php
@@ -5,13 +5,20 @@ namespace Streamly\Enum;
 class Level
 {
 	public const CRITICAL = 'critical';
+	public const HIGH = 'high';
 	public const NORMAL = 'normal';
+	public const LOW = 'low';
 
 	/**
 	 * @return string[]
 	 */
 	public static function all(): array
 	{
-		return [Level::NORMAL, Level::CRITICAL];
+		return [
+			Level::CRITICAL,
+			Level::HIGH,
+			Level::NORMAL,
+			Level::LOW,
+		];
 	}
 }
