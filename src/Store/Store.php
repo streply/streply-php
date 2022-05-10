65b78812b8118f70f18e6222075ea4dd8e90b1ee
Michal Molenda <contact@codeapps.io>
2022-05-10 22:15:11 +0200

    Filter before send option

--- src/Store/Store.php
+++ src/Store/Store.php
@@ -6,6 +6,7 @@ use Streamly\Store\Providers\StoreProviderInterface;
 use Streamly\Entity\Event;
 use Streamly\Entity\EntityInterface;
 use Streamly\Exceptions\InvalidRequestException;
+use Streamly\Streamly;
 
 class Store
 {
@@ -28,10 +29,24 @@ class Store
 	 */
 	public function push(EntityInterface $event): void
 	{
+		// Validation
 		$validationError = $event->getValidationError();
 
 		if($validationError !== null) {
 			throw new InvalidRequestException($validationError);
+		}
+
+		// Filter before send
+		if(Streamly::getOptions()->has('filterBeforeSend') && $event instanceof Event) {
+			$filterBeforeSend = Streamly::getOptions()->get('filterBeforeSend');
+
+			if(is_callable($filterBeforeSend)) {
+				$filterBeforeSendOutput = $filterBeforeSend($event);
+
+				if($filterBeforeSendOutput === false) {
+					return;
+				}
+			}
 		}
 
 		$this->storeProvider->push($event);
