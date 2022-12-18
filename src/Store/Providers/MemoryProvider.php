77cdb1411a8c8551ee3913313c9f151a7866d8a3
Michal Molenda <contact@codeapps.io>
2022-12-18 09:45:56 +0100

    Filter before send in Memory store

--- src/Store/Providers/MemoryProvider.php
+++ src/Store/Providers/MemoryProvider.php
@@ -6,6 +6,7 @@ use Streply\Exceptions\StreplyException;
 use Streply\Entity\EntityInterface;
 use Streply\Store\Providers\RequestProvider;
 use Streply\Request\Request;
+use Streply\Streply;
 
 class MemoryProvider implements StoreProviderInterface
 {
@@ -38,6 +39,19 @@ class MemoryProvider implements StoreProviderInterface
 	public function close(string $traceId): void
 	{
 		foreach($this->events as $event) {
+			// Filter before send
+			if(Streply::getOptions()->has('filterBeforeSend')) {
+				$filterBeforeSend = Streply::getOptions()->get('filterBeforeSend');
+
+				if(is_callable($filterBeforeSend)) {
+					$filterBeforeSendOutput = $filterBeforeSend($event);
+
+					if($filterBeforeSendOutput === false) {
+						continue;
+					}
+				}
+			}
+
 			Request::execute($event->toJson());
 		}
 	}
